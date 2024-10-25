38206e1604586bfdf999d367fe48db7c09fd80e5
Melisa Selin Welfringer <mwelfrin@c2r2s2.42luxembourg.lu>
2024-10-25 17:27:19 +0200

    oh lala

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: mwelfrin <mwelfrin@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 10:36:04 by mwelfrin          #+#    #+#             */
-/*   Updated: 2024/10/24 11:53:39 by mwelfrin         ###   ########.fr       */
+/*   Updated: 2024/10/25 17:25:44 by mwelfrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,6 @@ int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
 int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_strlen(char *s);
 #endif //LIBFT

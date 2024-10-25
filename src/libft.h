8a2740db121dedee0fe1807c6686a5cf83a100ca
Melisa Selin Welfringer <mwelfrin@c2r2s2.42luxembourg.lu>
2024-10-25 17:03:07 +0200

    innit

---
+++ src/libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwelfrin <mwelfrin@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 10:36:04 by mwelfrin          #+#    #+#             */
+/*   Updated: 2024/10/24 11:53:39 by mwelfrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT
+#define LIBFT
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+#endif //LIBFT
